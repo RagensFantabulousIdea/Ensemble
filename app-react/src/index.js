b15c389ddfa544d15f32e9413477be84f595299f
MANPREET <mondair.manpreet@gmail.com>
2017-04-04 11:16:05 -0400

    fxed router

---
+++ app-react/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import Routes from './Routes'
+// import Products from './Products';
+
+ReactDOM.render(
+  <Routes />,
+  document.getElementById('root')
+);
