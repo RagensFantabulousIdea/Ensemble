1573a685f76c9dff60d642465594b729d2a57a21
CosmicCandi <kalea.wolff@gmail.com>
2017-04-04 16:00:27 -0400

    Debugging why projects don't show up for current_user.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,36 @@
 class ProjectsController < ApplicationController
+
+
+  def index
+    @user = current_user
+    @projects = @user.projects.all
+      if @projects.empty?
+        render json: ["No projects to display. Create one?"], status: 200
+      else
+        render json: @projects
+      end
+  end
+
+  def create
+    @project = current_user.projects.new(project_params)
+    if @project.save
+      render json: @project
+    else
+      render json: @project.errors.full_messages, status: 400
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def project_params
+    params.permit(:title, :description, :author, :project_num)
+  end
+
+
 end
