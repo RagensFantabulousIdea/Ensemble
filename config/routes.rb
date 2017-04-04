d92a1546b973ec66549402dae2462dbf239c09d7
CosmicCandi <kalea.wolff@gmail.com>
2017-04-04 11:07:20 -0400

    Adds routes for login, and logout.

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
     resources :users
   end
 
+  get '/login' => 'sessions#new', as: :login
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy', as: :logout
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
