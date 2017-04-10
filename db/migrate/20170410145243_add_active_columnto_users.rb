2f89cb8aafd2668b1800555075fdf835b22ae0f0
CosmicCandi <kalea.wolff@gmail.com>
2017-04-10 13:40:32 -0400

    Adds ability to soft-delete a user.

---
+++ db/migrate/20170410145243_add_active_columnto_users.rb
@@ -0,0 +1,5 @@
+class AddActiveColumntoUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :active, :boolean, default: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406160326) do
+ActiveRecord::Schema.define(version: 20170410145243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,8 +99,9 @@ ActiveRecord::Schema.define(version: 20170406160326) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "token"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean  "active", default: true
   end
 
   add_foreign_key "comments", "users"
