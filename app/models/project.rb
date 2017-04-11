020097435ba97888087fb957e26f2e45b2bc723d
CosmicCandi <kalea.wolff@gmail.com>
2017-04-10 21:42:25 -0400

    Adds ability to delete projects.

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,11 +3,11 @@ class Project < ApplicationRecord
 
   belongs_to :owner, class_name: "User", foreign_key: :user_id
 
-  has_many :memberships
-  has_many :members, through: :memberships, source: :user
+  has_many :memberships, dependent: :delete_all
+  has_many :members, through: :memberships, source: :user, dependent: :delete_all
 
-  has_many :comments
-  has_many :left_comments, as: :commentable, class_name: 'Comment'
+  has_many :comments, dependent: :destroy
+  has_many :left_comments, as: :commentable, class_name: 'Comment', dependent: :destroy
 
   validates_presence_of :title, :description, :author, required: true
 
