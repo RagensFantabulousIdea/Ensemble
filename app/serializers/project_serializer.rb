b593c57f710dc80008efb9af5a4b963f49f687c1
CosmicCandi <kalea.wolff@gmail.com>
2017-04-16 18:32:43 -0400

    Fixes token in serializer to prevent 500 error. Could have sworn I'd already fixed this.

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -5,7 +5,12 @@ class ProjectSerializer < ActiveModel::Serializer
   has_many :left_comments
   has_many :assets
 
+<<<<<<< Updated upstream
   attributes :id, :token, :title, :description, :author, :project_num, :complete,
   :delayed, :inactive, :memberships
+=======
+  attributes :id, :project_token, :title, :description, :author, :project_num, :complete,
+  :delayed, :inactive
+>>>>>>> Stashed changes
 
 end
