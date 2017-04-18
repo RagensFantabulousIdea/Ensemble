fcd11bb8863fa8e03b03edcfac87bfeef20bf7fc
Ragen Sanner <ragens9@att.net>
2017-04-18 11:06:05 -0400

    added photos to create card, not yet working

--- app-react/src/components/SampleUploader.js
+++ app-react/src/components/SampleUploader.js
@@ -14,8 +14,6 @@ class SampleUploader extends React.Component {
   upload() {
     var data = new FormData()
     data.append('image', this.state.image)
-    data.append('frame_num', this.state.frame_num)
-    // data.append('api_token', sessionStorage.getItem('phetchly'))
 
     fetch('/api/projects/' + this.props.params.projectId + '/assets/' + this.props.params.assetId + '/photos?token=' + sessionStorage.getItem('token'), {
       method: 'POST',
@@ -31,11 +29,6 @@ class SampleUploader extends React.Component {
     return <div>
       <div className="form-group">
         <input type="file" name="image" className="form-control image" required onChange={(e) => this.setState({image:e.target.files[0]})} />
-      </div>
-
-      <div className="form-group">
-        <label htmlFor="frame_num" className="photoFrameInstructions">Enter the Frame Number for Each Photo Uploaded</label>
-        <input type="text" name="frame_num" className="form-control" required maxLength="255" value={this.state.frame_num} onChange={(e) => this.setState({frame_num:e.target.value})} />
       </div>
 
       <div className="form-group">
